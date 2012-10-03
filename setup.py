18cbf0225bea80a710f139a3ea99b2857d93c441
Pascal Volk <user@localhost.localdomain.org>
2012-10-03 12:51:24 +0000

    Released vmm-0.6.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 from distutils.core import setup
 from distutils.dist import DistributionMetadata
 
-VERSION = '0.6.0'
+VERSION = '0.6.1'
 
 descr = 'Tool to manage mail domains/accounts/aliases for Dovecot and Postfix'
 long_description = """
